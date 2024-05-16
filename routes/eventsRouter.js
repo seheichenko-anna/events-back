fe418c53fcbdc36b555e2faf22067f052db4f0da
seheichenko-anna <seheichenko.anna@gmail.com>
2024-05-16 13:28:08 +0300

    Add comment

--- routes/eventsRouter.js
+++ routes/eventsRouter.js
@@ -9,17 +9,17 @@ const eventsRouter = express.Router();
 
 eventsRouter.get("/", eventsControllers.getAllEvents);
 
-eventsRouter.get(
-  "events/:id/participants",
-  isValidId,
-  eventsControllers.getParticipants
-);
+// eventsRouter.get(
+//   "events/:id/participants",
+//   isValidId,
+//   eventsControllers.getParticipants
+// );
 
-eventsRouter.post(
-  "/events/:id/register",
-  isEmptyBody,
-  validateBody(regSchema),
-  eventsControllers.createParticipant
-);
+// eventsRouter.post(
+//   "/events/:id/register",
+//   isEmptyBody,
+//   validateBody(regSchema),
+//   eventsControllers.createParticipant
+// );
 
 export default eventsRouter;
